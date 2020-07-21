285c00ac0b39a715fbb90f5443c93e5316e5488c
IswaryaNidadavolu <iswaryanidadavolu@gmail.com>
2020-07-21 18:24:23 -0400

    commit

---
+++ sum.py
@@ -0,0 +1,10 @@
+a=int(input("enter the number"))
+b=int(input("enter the number"))
+c=int(input("enter the number"))
+result=a+b+c
+result1=(result)*3
+if a==b==c:
+    print(result1)
+else:
+    print(result)
+

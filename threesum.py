a887aad29a2b3b6a9f056ee59b88a005c6501d09
IswaryaNidadavolu <iswaryanidadavolu@gmail.com>
2020-07-23 15:29:25 -0400

    commit

---
+++ threesum.py
@@ -0,0 +1,9 @@
+n1=int(input("enter the number"))
+n2=int(input("enter the number"))
+n3=int(input("enter the number"))
+
+if ((n1==n2) or (n1==n3) or (n2==n3)):
+    result=0
+else:
+        result=n1+n2+n3
+print(result)

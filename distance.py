6c1429c20aa7f6c64be8c2179c6aea6e0b93a9df
IswaryaNidadavolu <iswaryanidadavolu@gmail.com>
2020-07-24 18:25:00 -0400

    python

---
+++ distance.py
@@ -0,0 +1,9 @@
+import math
+
+x1=int(input("enter the x1\t"))
+x2=int(input("enter the x2\t"))
+y1=int(input("enter the y1\t"))
+y2=int(input("enter the y2\t"))
+
+result=math.sqrt( ((x1-x2)**2)+((y1-y2)**2) )
+print(result)

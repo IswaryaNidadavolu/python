db9656fe90e5492a84eb34e7fac8d620f51c78e4
IswaryaNidadavolu <iswaryanidadavolu@gmail.com>
2020-07-24 18:14:05 -0400

    commit

---
+++ sort.py
@@ -0,0 +1,7 @@
+a=int(input("enter the number"))
+b=int(input("enter the number"))
+c=int(input("enter the number"))
+result=min(a,b,c)
+result1=max(a,b,c)
+final=(a+b+c) -result-result1
+print(result, final,result1)
